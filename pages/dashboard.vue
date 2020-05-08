4730fca05d12ba1b767660780067fc4ad1ecfc01
jonathanTQZ <56819545+jonathanTQZ@users.noreply.github.com>
2020-05-08 15:07:08 +0200

    -

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,111 +1,88 @@
 <template>
     
     <div id="dashboard">
-        <h1>Dashboard</h1>
+        <div class="container-quotes-list">
+            <h2 class="title" @click="showQuotesList(0)">Demandes en attentes (2)</h2>
 
-        <div>
-            <h2>Listes des demandes en attentes</h2>
-
-            <table>
-                <thead>
-                    <tr>
-                       <th>Date</th>
-                       <th></th>
-                    </tr>
-                </thead>
-
+            <table class="quotes-list" id="quotes-list-0">
                 <tbody>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                 </tbody>
             </table>
         </div>
 
-        <div>
-            <h2>Listes des demandes en approuvées</h2>
+        <div class="container-quotes-list">
+            <h2 class="title" @click="showQuotesList(1)">Demandes approuvées (1)</h2>
 
-            <table>
-                <thead>
-                    <tr>
-                       <th>Date</th>
-                       <th></th>
-                    </tr>
-                </thead>
-
+            <table class="quotes-list" id="quotes-list-1">
                 <tbody>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                 </tbody>
             </table>
         </div>
 
-        <div>
-            <h2>Listes des demandes refusées</h2>
+        <div class="container-quotes-list">
+            <h2 class="title" @click="showQuotesList(2)">Demandes refusées (4)</h2>
 
-            <table>
-                <thead>
-                    <tr>
-                       <th>Date</th>
-                       <th></th>
-                    </tr>
-                </thead>
-
+            <table class="quotes-list" id="quotes-list-2">
                 <tbody>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                     <tr>
                         <td>01/05/2020</td>
-                        <td><n-link to="/quote/1">Voir</n-link></td>
+                        <td class="actions"><n-link to="/quote/1">Voir</n-link></td>
                     </tr>
                 </tbody>
             </table>
@@ -125,7 +102,10 @@
             }
         },
         methods: {
-           
+           showQuotesList(index) {
+                let list = document.getElementById('quotes-list-'+index)
+                list.classList.toggle("show")
+            }
         }
     }
 
