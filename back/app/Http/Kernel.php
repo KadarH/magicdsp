306fc67595fa4c7c7fe2bb7bf87a96023e593863
jonathanTQZ <56819545+jonathanTQZ@users.noreply.github.com>
2020-06-05 10:47:43 +0200

    Update Kernel.php

--- back/app/Http/Kernel.php
+++ back/app/Http/Kernel.php
@@ -43,7 +43,6 @@ class Kernel extends HttpKernel
             EnsureFrontendRequestsAreStateful::class,
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            'auth:sanctum',
         ],
     ];
 
