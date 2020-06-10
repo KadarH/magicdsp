25ba55b5937decc54d068d2f872c3d621c411b27
jonathanTQZ <56819545+jonathanTQZ@users.noreply.github.com>
2020-06-10 17:07:21 +0200

    -

--- front/middleware/authenticated.js
+++ front/middleware/authenticated.js
@@ -1,7 +1,6 @@
 export default function ({ redirect, app }) {
     let auth = app.$cookies.get('auth')
 
-
     if ( auth === null || auth === undefined ) {
         return redirect('/login')
     }
