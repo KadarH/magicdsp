98ee9101809c20797b8e7b17334efe98f21c4f5d
jonathanTQZ <56819545+jonathanTQZ@users.noreply.github.com>
2020-07-13 09:56:32 +0200

    Update cors.php

--- back/config/cors.php
+++ back/config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['https://app.magic-dsp.com/api*', 'https://app.magic-dsp.com/sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://app.magic-dsp.com'],
 
     'allowed_origins_patterns' => [],
 
