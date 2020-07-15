4b1faf3b54cdfad3241ca3899c7c3098b94abd9f
jonathanTQZ <56819545+jonathanTQZ@users.noreply.github.com>
2020-07-15 13:25:06 +0200

    URL reset password front
    https://app.asana.com/0/1177996616012308/1184292816730697

--- back/resources/views/mails/password_reset.blade.php
+++ back/resources/views/mails/password_reset.blade.php
@@ -1,6 +1,6 @@
 <h1>Réinitialisation de votre mot de passe</h1>
 <p>Bonjour, vous avez fait une demande pour réinitialiser votre mot de passe. Vous pouvez dès à présent le faire en cliquant sur le lien (valide pendant 1h) ci-dessous.</p>
-<a class="link-reset" href="http://localhost:3000/password/reset/{{$user->password_reset_token}}">Réinitialisation du mot de passe</a>
+<a class="link-reset" href="https://app.magic-dsp.com/password/reset/{{$user->password_reset_token}}">Réinitialisation du mot de passe</a>
 <p>Si vous n'êtes pas à l'origine de cette demande, veuillez ne pas tenir compte de cet email. Votre ancien mot de passe reste inchangé et vous pouvez continuer à l'utiliser.</p>
 
 <style>
