2d93825a69a900338bab3cfe29990b9c4b7d62f2
jonathanTQZ <56819545+jonathanTQZ@users.noreply.github.com>
2020-07-22 14:26:06 +0200

    add brands and models tables

---
+++ back/database/migrations/2020_07_17_130508_create_models_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('models', function (Blueprint $table) {
+            $table->id();
+            $table->text('name')->nullable();
+            $table->foreignId('brand_id')->references('id')->on('brands');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('models');
+    }
+}
