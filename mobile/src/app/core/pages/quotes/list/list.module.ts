e1256c3386090d1af1f4f6bc3e8c2e4aacde8ef7
kadarH <kadar.hamza@gmail.com>
2020-10-29 23:48:37 +0100

    feat(loading): externalise list loading to new module

--- mobile/src/app/core/pages/quotes/list/list.module.ts
+++ mobile/src/app/core/pages/quotes/list/list.module.ts
@@ -7,14 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { ListPageRoutingModule } from './list-routing.module';
 
 import { ListPage } from './list.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ListLoadingComponent } from 'src/app/shared/components/list-loading/list-loading.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ListPageRoutingModule
+    ListPageRoutingModule,
+    SharedModule
   ],
-  declarations: [ListPage]
+  declarations: [ListPage, ListLoadingComponent]
 })
 export class ListPageModule {}
---
+++ mobile/src/app/shared/components/list-loading/list-loading.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-loading',
+  templateUrl: './list-loading.component.html',
+  styleUrls: ['./list-loading.component.scss'],
+})
+export class ListLoadingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
