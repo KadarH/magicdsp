cdc365b96a42384b8b48f1c8a0c4194dd634e56d
kadarH <kadar.hamza@gmail.com>
2020-11-11 01:16:18 +0100

    feat(admin): add list of quotes by user type

--- mobile/src/app/core/pages/admin/devis/devis.module.ts
+++ mobile/src/app/core/pages/admin/devis/devis.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { DevisPageRoutingModule } from './devis-routing.module';
 
 import { DevisPage } from './devis.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    DevisPageRoutingModule
+    SharedModule,
+    DevisPageRoutingModule,
   ],
-  declarations: [DevisPage]
+  declarations: [DevisPage],
 })
 export class DevisPageModule {}
--- mobile/src/app/core/pages/admin/devis/devis.page.ts
+++ mobile/src/app/core/pages/admin/devis/devis.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { Quote } from 'src/app/core/models/quote';
+import { LoaderService } from 'src/app/shared/services/loader.service';
+import { ToastService } from 'src/app/shared/services/toast.service';
+import { QuotesService } from '../../quotes/services/quotes.service';
 
 @Component({
   selector: 'app-devis',
@@ -6,10 +11,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./devis.page.scss'],
 })
 export class DevisPage implements OnInit {
+  status: string;
 
-  constructor() { }
+  quotes: Quote[];
+
+  constructor(
+    private quotesService: QuotesService,
+    private loaderService: LoaderService,
+    private toastService: ToastService
+  ) {}
 
   ngOnInit() {
+    this.status = '1';
+    this.segmentChanged(null);
   }
 
+  segmentChanged(ev: any) {
+    this.quotes = null;
+    this.quotesService
+      .getQuotesByUserStatus(this.status)
+      .pipe(take(1))
+      .subscribe((res: any) => {
+        if (res.success) {
+          this.quotes = res.data.quotes;
+        }
+      });
+  }
+
+  refuseQuote(quote: any) {
+    this.loaderService.presentLoading();
+    this.quotesService
+      .refuseQuote(quote)
+      .pipe(take(1))
+      .subscribe(
+        (res: any) => {
+          if (res.success) {
+            this.quotes = this.quotes.filter((obj) => obj.id !== quote.id);
+            this.loaderService.dismiss();
+            this.toastService.presentToast(
+              'Le devis ' + quote.id + 'a été refusé.'
+            );
+          } else {
+            this.loaderService.dismiss();
+            this.toastService.presentToast('Erreur: opération échouée');
+          }
+        },
+        (err) => {
+          this.loaderService.dismiss();
+          this.toastService.presentToast('Erreur: opération échouée');
+        }
+      );
+  }
+
+  deleteQuote(quote: Quote) {
+    this.loaderService.presentLoading();
+    this.quotesService
+      .deleteQuote(quote)
+      .pipe(take(1))
+      .subscribe(
+        (res: any) => {
+          if (res.success) {
+            this.quotes = this.quotes.filter((obj) => obj.id !== quote.id);
+            this.loaderService.dismiss();
+            this.toastService.presentToast(
+              'Le devis ' + quote.id + 'a été refusé.'
+            );
+          } else {
+            this.loaderService.dismiss();
+            this.toastService.presentToast('Erreur: opération échouée');
+          }
+        },
+        (err) => {
+          this.loaderService.dismiss();
+          this.toastService.presentToast('Erreur: opération échouée');
+        }
+      );
+  }
 }
