c9cf9b1e8262bda0fcebfb84bcb4155f2a7f7014
kadarH <kadar.hamza@gmail.com>
2020-11-18 23:13:41 +0100

    fix(meeting): fix meeting reservation and availabilities

--- mobile/src/app/core/pages/quotes/services/garages.service.ts
+++ mobile/src/app/core/pages/quotes/services/garages.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -25,6 +25,33 @@ export class GaragesService {
   deleteGarage(garage: any) {
     return this.http.delete<any>(
       `${this.AUTH_SERVER_ADDRESS}admin/garages/` + garage.id
+    );
+  }
+  checkAvailabilities(quoteId: number, garageId: number, date: string) {
+    let params = new HttpParams();
+
+    params = params.append('date', date);
+    params = params.append('quote_id', quoteId + '');
+
+    return this.http.get<any>(
+      `${this.AUTH_SERVER_ADDRESS}garages/` + garageId + '/availabilities',
+      { params }
+    );
+  }
+
+  fixMeeting(
+    quoteId: number,
+    garageId: number,
+    dateSelected: string,
+    hourSelected: string
+  ) {
+    return this.http.patch(
+      `${this.AUTH_SERVER_ADDRESS}quotes/` + quoteId + '/meetings',
+      {
+        date: dateSelected,
+        hour: hourSelected,
+        garage_id: garageId,
+      }
     );
   }
 }
--- mobile/src/app/core/pages/quotes/show/meeting/meeting.page.ts
+++ mobile/src/app/core/pages/quotes/show/meeting/meeting.page.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { GaragesService } from '../../services/garages.service';
+import * as moment from 'moment';
+import { AlertController } from '@ionic/angular';
+import { ToastService } from 'src/app/shared/services/toast.service';
+import { LoaderService } from 'src/app/shared/services/loader.service';
 
 @Component({
   selector: 'app-meeting',
@@ -7,11 +15,158 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./meeting.page.scss'],
 })
 export class MeetingPage implements OnInit {
-  id: number;
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  meetingForm: FormGroup;
+  garages$: Observable<any[]>;
+  availabilities: any;
+  quoteId: number;
+  garageId: number;
+  dateSelected: string;
+  hourSelected: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private formbuilder: FormBuilder,
+    private garageService: GaragesService,
+    private alertController: AlertController,
+    private toastService: ToastService,
+    private loaderService: LoaderService
+  ) {}
 
   ngOnInit() {
-    const devisId = this.route.snapshot.params.id;
-    this.id = this.route.snapshot.params.id;
+    this.quoteId = this.route.snapshot.params.id;
+
+    this.garages$ = this.garageService.getGarages().pipe(
+      take(1),
+      map((res) => res.data.garages)
+    );
+
+    this.meetingForm = this.formbuilder.group({
+      garage: ['', Validators.required],
+      date: [null, Validators.required],
+    });
+
+    this.meetingForm.get('garage').valueChanges.subscribe((value) => {
+      this.garageId = value.id;
+    });
+
+    this.meetingForm.get('date').valueChanges.subscribe((value) => {
+      const date = moment(value).format('YYYY-MM-DD');
+      this.dateSelected = date;
+      if (!value) {
+        this.availabilities = null;
+      } else {
+        this.loaderService.presentLoading();
+
+        this.garageService
+          .checkAvailabilities(this.quoteId, this.garageId, date)
+          .pipe(take(1))
+          .subscribe(
+            (res) => {
+              if (res.success) {
+                this.availabilities = res.data.garage.availabilities;
+
+                this.loaderService.dismiss();
+              } else {
+                this.toastService.presentToast(
+                  'Probléme lors du chargement de la liste des heures disponibles'
+                );
+                this.loaderService.dismiss();
+              }
+            },
+            (err) => {
+              this.toastService.presentToast(
+                'Probléme lors du chargement de la liste des heures disponibles'
+              );
+              this.loaderService.dismiss();
+            }
+          );
+      }
+    });
+  }
+
+  selectHour(event: any, index: string) {
+    const hours = document.querySelectorAll('.availability .content');
+    hours.forEach((hour) => {
+      hour.classList.remove('selected');
+    });
+    event.target.classList.add('selected');
+    this.hourSelected = index;
+  }
+
+  fixMeeting() {
+    this.presentAlertConfirm(
+      moment(this.dateSelected).format('DD-MM-YYYY'),
+      this.hourSelected
+    );
+  }
+
+  patchQuote() {
+    this.loaderService.presentLoading();
+    this.garageService
+      .fixMeeting(
+        this.quoteId,
+        this.garageId,
+        this.dateSelected,
+        this.hourSelected
+      )
+      .pipe(take(1))
+      .subscribe(
+        (res: any) => {
+          if (res.success) {
+            console.log(res);
+            this.toastService.presentToast(
+              'Le rendez-vous a été ajouté avec succès'
+            );
+            this.loaderService.dismiss();
+            this.router.navigateByUrl('/quotes/show/' + this.quoteId);
+          } else {
+            this.toastService.presentToast(
+              'Operation echoué, probléme du serveur'
+            );
+            this.loaderService.dismiss();
+          }
+        },
+        (err) => {
+          this.toastService.presentToast(
+            'Operation echoué, probléme du serveur'
+          );
+          this.loaderService.dismiss();
+        }
+      );
+  }
+  // Easy access for form fields
+  get garage() {
+    return this.meetingForm.get('garage');
+  }
+
+  async presentAlertConfirm(date: string, hour: string) {
+    const alert = await this.alertController.create({
+      header: 'Confirmation!',
+      message:
+        "Etês-vous sûr d'avoir choisi un rendez- vous le " +
+        date +
+        ' à ' +
+        hour +
+        ' ?',
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirmation Canceled');
+          },
+        },
+        {
+          text: 'Confirmer',
+          handler: () => {
+            this.patchQuote();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 }
